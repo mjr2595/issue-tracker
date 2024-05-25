94c8fbc60e8532a741d2335a77f8cf2f7ebba73b
mjr2595 <michaeljohnraymond@gmail.com>
2024-05-25 11:23:38 -0500

    feat: Secure authentication logic

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,11 +1,16 @@
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/validationSchemas";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   // Validate the request body
   const body = await request.json();
   const validation = issueSchema.safeParse(body);
@@ -37,6 +42,9 @@ export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: string } }
 ) {
+  const session = await getServerSession(authOptions);
+  if (!session) return NextResponse.json({}, { status: 401 });
+
   // Find the issue in the database
   const issue = await prisma.issue.findUnique({
     where: { id: parseInt(params.id) },
